36a46e415cd25a25aa3d5e27aeaf34a5e1cbb389
angad <angadsharma1016@gmail.com>
2018-05-08 15:45:09 +0530

    secret.js

---
+++ secret.js
@@ -0,0 +1,13 @@
+//add your Oauth API credentials and mongo URL here
+
+module.exports = {
+    mongoURL:"",
+    salt:10,
+    secretKey:"",
+    gclientID:'',
+    gclientSecret:'',
+    gcallbackURL:'',
+    fclientID:'',
+    fclientSecret:'',
+    fcallbackURL:''
+};
